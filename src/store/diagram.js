3eb89e177655caa07578942779848acf7dcca731
Alec Aivazis <alec@aivazis.com>
2016-12-25 22:53:22 -0800

    diagram reducer responds to SET_TITLE and TOGGLE_GRID actions

--- src/store/diagram.js
+++ src/store/diagram.js
@@ -1,9 +1,32 @@
+// local imports
+import { SET_TITLE, TOGGLE_GRID } from 'actions/diagram'
+
+// the default state
 export const initialState = {
     title: 'An Example Diagram',
+    showGrid: true,
 }
 
 //
 export default (state = initialState, {type, payload}) => {
+
+    // if the action means we should toggle the grid
+    if (type === TOGGLE_GRID) {
+        // invert the {showGrid} state variable
+        return {
+            ...state,
+            showGrid: !state.showGrid,
+        }
+    }
+
+    // if the action indicates a change in title
+    if (type === SET_TITLE) {
+        // change the title
+        return {
+            ...state,
+            title: payload,
+        }
+    }
 
     // its an action we don't recognize
     return state
--- src/store/tests/diagram.test.js
+++ src/store/tests/diagram.test.js
@@ -2,6 +2,10 @@
 import {combineReducers, createStore} from 'redux'
 // local imports
 import diagramReducer, { initialState } from '../diagram'
+import {
+    SET_TITLE, setDiagramTitle,
+    TOGGLE_GRID, toggleGrid,
+} from 'actions/diagram'
 
 describe('diagram reducer', function() {
     it('has a reasonable default', function() {
@@ -9,5 +13,23 @@ describe('diagram reducer', function() {
         const val = diagramReducer(undefined, {type: "init"})
         // expect the default initial state
         expect(val).to.deep.equal(initialState)
+    })
+
+    it('responds to the SET_TITLE action', function() {
+        // create an action corresponding to changing the title
+        const action = setDiagramTitle('hello')
+        // get the mutated state after changing the title
+        const state = diagramReducer(undefined, action)
+        // make sure the title matches the expectation
+        expect(state.title).to.equal('hello')
+    })
+
+    it('responds to the TOGGLE_GRID action', function() {
+        // create an action corresponding to changing the title
+        const action = toggleGrid()
+        // get the mutated state after changing the title
+        const state = diagramReducer(undefined, action)
+        // make sure the title matches the expectation
+        expect(state.showGrid).to.equal(!initialState.showGrid)
     })
 })
