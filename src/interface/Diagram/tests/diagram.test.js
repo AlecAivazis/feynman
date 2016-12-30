e1aafeb8dc57f3d0645474b933e0f5caf127d91e
Alec Aivazis <alec@aivazis.com>
2016-12-29 16:32:15 -0800

    added tests for diagram grid interaction

--- src/interface/Diagram/tests/diagram.test.js
+++ src/interface/Diagram/tests/diagram.test.js
@@ -1,10 +1,55 @@
 // external imports
 import React from 'react'
+import { Provider } from 'react-redux'
+import { mount } from 'enzyme'
+// local imports
+import { createStore } from 'store'
+import { toggleGrid } from 'actions/info'
+import Diagram from '..'
+import Grid from '../Grid'
 
 describe('Diagram Components', function() {
+
     describe('Diagram', function() {
+
         it('Shows the grid when the store says so', function() {
             // create a verion of the store
+            const store = createStore()
+            // if the grid is supposed to be hidden
+            if (!store.getState().info.showGrid) {
+                // show the grid
+                store.dispatch(toggleGrid())
+            }
+
+            // render the diagram in the wrapper
+            const wrapper = mount(
+                <Provider store={store}>
+                    <Diagram/>
+                </Provider>
+            )
+
+            // make sure there is a grid
+            expect(wrapper.find(Grid)).to.have.length(1)
+        })
+
+        it('Hides the grid when the store says so', function() {
+            // create a verion of the store
+            const store = createStore()
+            // if the grid is supposed to be hidden
+            if (store.getState().info.showGrid) {
+                // show the grid
+                store.dispatch(toggleGrid())
+            }
+
+            // render the diagram in the wrapper
+            const wrapper = mount(
+                <Provider store={store}>
+                    <Diagram/>
+                </Provider>
+            )
+
+            // make sure there is a grid
+            expect(wrapper.find(Grid)).to.have.length(0)
         })
     })
 })
