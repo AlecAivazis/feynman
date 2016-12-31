1e72f5f4250ff50be1af36b667b26b350fc54877
Alec Aivazis <alec@aivazis.com>
2016-12-30 21:08:00 -0800

    cleaned up propagator rendering logic

--- src/interface/Diagram/index.jsx
+++ src/interface/Diagram/index.jsx
@@ -15,21 +15,12 @@ const Diagram = ({info, elements, style}) => {
     // figure the concrete locations for each propgators (dereference the anchors)
     const propagators = propagatorsWithLocation(elements)
 
-    // the list of anchors
-    const anchors = Object.keys(elements.anchors).map(anchorId => {
-        // retrieve the anchor
-        const anchor = elements.anchors[anchorId]
-
-        // render the component
-        return <Anchor {...anchor} key={anchor.id}/>
-    })
-
     // render the various components of the diagram
     return (
         <svg style={{...elementStyle, ...style}}>
             {info.showGrid && <Grid/>}
             {propagators.map((element, i) => <Propagator {...element} key={i}/>)}
-            {info.showAnchors && anchors}
+            {info.showAnchors && Object.values(elements.anchors).map(anchor => <Anchor {...anchor} key={anchor.id} />)}
         </svg>
     )
 }
