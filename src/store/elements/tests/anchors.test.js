7233f11f6b369a0ab4ea593e3644cf02a06b7e32
Alec Aivazis <alec@aivazis.com>
2017-01-04 11:47:31 -0800

    added more organization to elements reducer test suite

--- src/store/elements/tests/anchors.test.js
+++ src/store/elements/tests/anchors.test.js
@@ -8,155 +8,157 @@ describe('Reducers', function() {
 
     describe('Elements reducer', function() {
 
-        // a store to test with
-        let store
-        // with one anchor
-        const initialAnchor = {
-            id: 1,
-            x: 50,
-            y: 100
-        }
-
-        beforeEach(function() {
-            // start with a fresh store
-            store = createStore()
-            // add a single propagator to the store
-            store.dispatch(addAnchors(initialAnchor))
-        })
-
-        it('responds to the ADD_ANCHOR reducer', function() {
-            // make sure the state matches expectation
-            expect(store.getState().elements.anchors).to.deep.equal({
-                1: {
-                    x: 50,
-                    y: 100,
-                    id: 1,
-                }
-            })
-        })
-
-        it('can add multiple anchors with ADD_ANCHOR', function() {
-            // the anchor to test
-            const anchors = [
-                {
-                    id: 2,
-                    x: 50,
-                    y: 100,
-                },
-                {
-                    id: 3,
-                    x: 100,
-                    y: 100
-                }
-            ]
-
-            // add the propagator to the store
-            store.dispatch(addAnchors(...anchors))
-
-            // make sure the state matches expectation
-            expect(store.getState().elements.anchors).to.deep.equal({
-                [initialAnchor.id]: initialAnchor,
-                [anchors[0].id]: anchors[0],
-                [anchors[1].id]: anchors[1],
-            })
-        })
-
-        it('barfs if there is an anchor id conflict', function() {
-
-            // the action to add the id-conflicting anchor
-            const action = () => store.dispatch(addAnchors({
+        describe('Anchors', function() {
+            // a store to test with
+            let store
+            // with one anchor
+            const initialAnchor = {
                 id: 1,
-                x: 100,
-                y: 100,
-            }))
-
-            // make sure the id-conflicting action throws an error
-            expect(action).to.throw(Error)
-        })
-
-        it('can move single anchor with SET_ANCHOR_LOCATIONS action', function() {
-            // the anchor to test
-            const move = {
-                id: initialAnchor.id,
                 x: 50,
-                y: 100,
+                y: 100
             }
 
-            // add the propagator to the store
-            store.dispatch(setAnchorLocations(move))
+            beforeEach(function() {
+                // start with a fresh store
+                store = createStore()
+                // add a single propagator to the store
+                store.dispatch(addAnchors(initialAnchor))
+            })
 
-            // make sure the initial anchor has been moved in the right direction
-            expect(store.getState().elements.anchors[initialAnchor.id].x).to.equal(move.x)
-            expect(store.getState().elements.anchors[initialAnchor.id].y).to.equal(move.y)
-        })
+            it('responds to the ADD_ANCHOR reducer', function() {
+                // make sure the state matches expectation
+                expect(store.getState().elements.anchors).to.deep.equal({
+                    1: {
+                        x: 50,
+                        y: 100,
+                        id: 1,
+                    }
+                })
+            })
 
-        it('can move multiple anchors with SET_ANCHOR_LOCATIONS action', function() {
-            const anchor = {
-                id: 2,
-                x: 50,
-                y: 50
-            }
-            // add a second anchor
-            store.dispatch(addAnchors(anchor))
+            it('can add multiple anchors with ADD_ANCHOR', function() {
+                // the anchor to test
+                const anchors = [
+                    {
+                        id: 2,
+                        x: 50,
+                        y: 100,
+                    },
+                    {
+                        id: 3,
+                        x: 100,
+                        y: 100
+                    }
+                ]
 
-            // the anchor to test
-            const moves = [
-                {
+                // add the propagator to the store
+                store.dispatch(addAnchors(...anchors))
+
+                // make sure the state matches expectation
+                expect(store.getState().elements.anchors).to.deep.equal({
+                    [initialAnchor.id]: initialAnchor,
+                    [anchors[0].id]: anchors[0],
+                    [anchors[1].id]: anchors[1],
+                })
+            })
+
+            it('barfs if there is an anchor id conflict', function() {
+
+                // the action to add the id-conflicting anchor
+                const action = () => store.dispatch(addAnchors({
+                    id: 1,
+                    x: 100,
+                    y: 100,
+                }))
+
+                // make sure the id-conflicting action throws an error
+                expect(action).to.throw(Error)
+            })
+
+            it('can move single anchor with SET_ANCHOR_LOCATIONS action', function() {
+                // the anchor to test
+                const move = {
                     id: initialAnchor.id,
                     x: 50,
                     y: 100,
-                },
-                {
-                    id: anchor.id,
+                }
+
+                // add the propagator to the store
+                store.dispatch(setAnchorLocations(move))
+
+                // make sure the initial anchor has been moved in the right direction
+                expect(store.getState().elements.anchors[initialAnchor.id].x).to.equal(move.x)
+                expect(store.getState().elements.anchors[initialAnchor.id].y).to.equal(move.y)
+            })
+
+            it('can move multiple anchors with SET_ANCHOR_LOCATIONS action', function() {
+                const anchor = {
+                    id: 2,
                     x: 50,
                     y: 50
                 }
-            ]
+                // add a second anchor
+                store.dispatch(addAnchors(anchor))
 
-            // add the propagator to the store
-            store.dispatch(setAnchorLocations(...moves))
+                // the anchor to test
+                const moves = [
+                    {
+                        id: initialAnchor.id,
+                        x: 50,
+                        y: 100,
+                    },
+                    {
+                        id: anchor.id,
+                        x: 50,
+                        y: 50
+                    }
+                ]
 
-            // make sure the initial anchor was moved in the right direction
-            expect(store.getState().elements.anchors[initialAnchor.id].x).to.equal(moves[0].x)
-            expect(store.getState().elements.anchors[initialAnchor.id].y).to.equal(moves[0].y)
-            // make sure the new anchor was moved in the right direction
-            expect(store.getState().elements.anchors[anchor.id].x).to.equal(moves[1].x)
-            expect(store.getState().elements.anchors[anchor.id].y).to.equal(moves[1].y)
-        })
+                // add the propagator to the store
+                store.dispatch(setAnchorLocations(...moves))
 
-        it('only moved the appropriate anchors', function() {
-            const anchor = {
-                id: 2,
-                x: 50,
-                y: 50
-            }
-            // add a second anchor
-            store.dispatch(addAnchors(anchor))
+                // make sure the initial anchor was moved in the right direction
+                expect(store.getState().elements.anchors[initialAnchor.id].x).to.equal(moves[0].x)
+                expect(store.getState().elements.anchors[initialAnchor.id].y).to.equal(moves[0].y)
+                // make sure the new anchor was moved in the right direction
+                expect(store.getState().elements.anchors[anchor.id].x).to.equal(moves[1].x)
+                expect(store.getState().elements.anchors[anchor.id].y).to.equal(moves[1].y)
+            })
 
-            // the move to test
-            const move = {
-                id: initialAnchor.id,
-                x: 50,
-                y: 100,
-            }
+            it('only moved the appropriate anchors', function() {
+                const anchor = {
+                    id: 2,
+                    x: 50,
+                    y: 50
+                }
+                // add a second anchor
+                store.dispatch(addAnchors(anchor))
 
-            // add the propagator to the store
-            store.dispatch(setAnchorLocations(move))
+                // the move to test
+                const move = {
+                    id: initialAnchor.id,
+                    x: 50,
+                    y: 100,
+                }
 
-            // make sure the other anchor was left unmodified
-            expect(store.getState().elements.anchors[anchor.id].x).to.equal(anchor.x)
-            expect(store.getState().elements.anchors[anchor.id].y).to.equal(anchor.y)
-        })
+                // add the propagator to the store
+                store.dispatch(setAnchorLocations(move))
 
-        it('barfs if moving with no id', function() {
-            // the move to test
-            const move = {
-                x: 50,
-                y: 100,
-            }
+                // make sure the other anchor was left unmodified
+                expect(store.getState().elements.anchors[anchor.id].x).to.equal(anchor.x)
+                expect(store.getState().elements.anchors[anchor.id].y).to.equal(anchor.y)
+            })
 
-            // add the propagator to the store
-            expect(() => store.dispatch(setAnchorLocations(move))).to.throw(Error, noIdErr)
+            it('barfs if moving with no id', function() {
+                // the move to test
+                const move = {
+                    x: 50,
+                    y: 100,
+                }
+
+                // add the propagator to the store
+                expect(() => store.dispatch(setAnchorLocations(move))).to.throw(Error, noIdErr)
+            })
         })
     })
 })
--- src/store/elements/tests/elements.test.js
+++ src/store/elements/tests/elements.test.js
@@ -11,179 +11,180 @@ import {
 
 
 describe('Reducers', function() {
-
     describe('Elements reducer', function() {
-        it('has a default value', function() {
-            // pass an undefined current state
-            const val = reducer(undefined, {type: "init"})
-            // expect the default initial state
-            expect(val).to.exist
-        })
+        describe('Misc', function() {
+            it('has a default value', function() {
+                // pass an undefined current state
+                const val = reducer(undefined, {type: "init"})
+                // expect the default initial state
+                expect(val).to.exist
+            })
 
-        it('responds to the SELECT_ELEMENTS action', function() {
-            // add some anchors
-            const addAction = addAnchors(
-                {
-                    id: 1,
+            it('responds to the SELECT_ELEMENTS action', function() {
+                // add some anchors
+                const addAction = addAnchors(
+                    {
+                        id: 1,
+                        x: 50,
+                        y: 100,
+                    },
+                    {
+                        id: 2,
+                        x: 100,
+                        y: 100,
+                    }
+                )
+                const state = reducer(undefined, addAction)
+
+                // the selection
+                const selection = [
+                    {
+                        type: 'anchors',
+                        id: 1
+                    },
+                    {
+                        type: 'anchors',
+                        id: 2
+                    }
+                ]
+                // create an action to fire
+                const selectAction = selectElements(...selection)
+
+                // pass the action to the reducer in the intermediate state
+                const val = reducer(state, selectAction)
+
+                // make sure the selection is present
+                expect(val.selection).to.deep.equal(selection)
+            })
+
+            it('throws an error if selecting an anchor that doesn\'t exist', function() {
+                // the selection
+                const selection = [
+                    {
+                        type: 'anchors',
+                        id: 1
+                    },
+                ]
+                // the action to throw
+                const action = selectElements(...selection)
+
+                expect(() => reducer(undefined, action)).to.throw(Error)
+            })
+
+            it('responds to the CLEAR_SELECTION action', function() {
+                // add some anchors
+                const addAction = addAnchors(
+                    {
+                        id: 1,
+                        x: 50,
+                        y: 100,
+                    }
+                )
+                const actionState = reducer(undefined, addAction)
+                // add some anchors
+                const selectAction = selectElements(
+                    {
+                        type: 'anchors',
+                        id: 1,
+                    }
+                )
+                const state = reducer(actionState, selectAction)
+                // sanity check
+                expect(state.selection).to.have.length(1)
+
+                // clear the selection
+                const clearedState = reducer(state, clearSelection())
+
+                // make sure the selection is emtpy
+                expect(clearedState.selection).to.have.length(0)
+            })
+
+            it('merges anchors that are located at the same location', function() {
+                // the location of the conflict
+                const coords = {
                     x: 50,
-                    y: 100,
-                },
-                {
-                    id: 2,
-                    x: 100,
-                    y: 100,
+                    y: 50,
                 }
-            )
-            const state = reducer(undefined, addAction)
+                // start off with some anchors
+                const anchorState = reducer(undefined, addAnchors(
+                    {
+                        id: 1,
+                        ...coords,
+                    },
+                    {
+                        id: 2,
+                        ...coords,
+                    },
+                    {
+                        id: 3,
+                        x: 300,
+                        y: 500,
+                    }
+                ))
 
-            // the selection
-            const selection = [
-                {
-                    type: 'anchors',
-                    id: 1
-                },
-                {
-                    type: 'anchors',
-                    id: 2
-                }
-            ]
-            // create an action to fire
-            const selectAction = selectElements(...selection)
+                // tell the reducer to merge itself
+                const mergedState = reducer(anchorState, mergeElements(1))
 
-            // pass the action to the reducer in the intermediate state
-            const val = reducer(state, selectAction)
+                // make sure there are only two anchors left
+                expect(Object.values(mergedState.anchors)).to.have.length(2)
 
-            // make sure the selection is present
-            expect(val.selection).to.deep.equal(selection)
-        })
+                // make sure the first entry does not exist
+                expect(mergedState.anchors[2].x).to.equal(coords.x)
+                expect(mergedState.anchors[2].y).to.equal(coords.y)
+                // make sure the second is valid
+                expect(mergedState.anchors[1]).to.not.exist
+                // make sure the third was left untouched
+                expect(mergedState.anchors[3].x).to.equal(300)
+                expect(mergedState.anchors[3].y).to.equal(500)
+            })
 
-        it('throws an error if selecting an anchor that doesn\'t exist', function() {
-            // the selection
-            const selection = [
-                {
-                    type: 'anchors',
-                    id: 1
-                },
-            ]
-            // the action to throw
-            const action = selectElements(...selection)
-
-            expect(() => reducer(undefined, action)).to.throw(Error)
-        })
-
-        it('responds to the CLEAR_SELECTION action', function() {
-            // add some anchors
-            const addAction = addAnchors(
-                {
-                    id: 1,
+            it('replaces anchor references when merging', function() {
+                // the location of the conflict
+                const coords = {
                     x: 50,
-                    y: 100,
+                    y: 50,
                 }
-            )
-            const actionState = reducer(undefined, addAction)
-            // add some anchors
-            const selectAction = selectElements(
-                {
-                    type: 'anchors',
-                    id: 1,
-                }
-            )
-            const state = reducer(actionState, selectAction)
-            // sanity check
-            expect(state.selection).to.have.length(1)
 
-            // clear the selection
-            const clearedState = reducer(state, clearSelection())
+                // start off with some anchors
+                const anchorState = reducer(undefined, addAnchors(
+                    {
+                        id: 1,
+                        ...coords,
+                    },
+                    {
+                        id: 2,
+                        ...coords,
+                    },
+                    {
+                        id: 3,
+                        x: 300,
+                        y: 500,
+                    }
+                ))
 
-            // make sure the selection is emtpy
-            expect(clearedState.selection).to.have.length(0)
-        })
+                // add a propagator
+                const propagatorState = reducer(anchorState, addPropagators(
+                    {
+                        type: 'fermion',
+                        anchor1: 2,
+                        anchor2: 3
+                    }
+                ))
 
-        it('merges anchors that are located at the same location', function() {
-            // the location of the conflict
-            const coords = {
-                x: 50,
-                y: 50,
-            }
-            // start off with some anchors
-            const anchorState = reducer(undefined, addAnchors(
-                {
-                    id: 1,
-                    ...coords,
-                },
-                {
-                    id: 2,
-                    ...coords,
-                },
-                {
-                    id: 3,
-                    x: 300,
-                    y: 500,
-                }
-            ))
+                // tell the store to merge elements onto anchor 1
+                const mergedState = reducer(propagatorState, mergeElements(1))
 
-            // tell the reducer to merge itself
-            const mergedState = reducer(anchorState, mergeElements(1))
+                // since that should replace anchor 1, the propagotr's anchor1 value should be 2
+                expect(mergedState.propagators[0].anchor1).to.equal(2)
+            })
 
-            // make sure there are only two anchors left
-            expect(Object.values(mergedState.anchors)).to.have.length(2)
+            it('barfs if merging onto an undefined id', function() {
+                expect(() => reducer(undefined, mergeElements(undefined))).to.throw(Error)
+            })
 
-            // make sure the first entry does not exist
-            expect(mergedState.anchors[2].x).to.equal(coords.x)
-            expect(mergedState.anchors[2].y).to.equal(coords.y)
-            // make sure the second is valid
-            expect(mergedState.anchors[1]).to.not.exist
-            // make sure the third was left untouched
-            expect(mergedState.anchors[3].x).to.equal(300)
-            expect(mergedState.anchors[3].y).to.equal(500)
-        })
-
-        it('replaces anchor references when merging', function() {
-            // the location of the conflict
-            const coords = {
-                x: 50,
-                y: 50,
-            }
-
-            // start off with some anchors
-            const anchorState = reducer(undefined, addAnchors(
-                {
-                    id: 1,
-                    ...coords,
-                },
-                {
-                    id: 2,
-                    ...coords,
-                },
-                {
-                    id: 3,
-                    x: 300,
-                    y: 500,
-                }
-            ))
-
-            // add a propagator
-            const propagatorState = reducer(anchorState, addPropagators(
-                {
-                    type: 'fermion',
-                    anchor1: 2,
-                    anchor2: 3
-                }
-            ))
-
-            // tell the store to merge elements onto anchor 1
-            const mergedState = reducer(propagatorState, mergeElements(1))
-
-            // since that should replace anchor 1, the propagotr's anchor1 value should be 2
-            expect(mergedState.propagators[0].anchor1).to.equal(2)
-        })
-
-        it('barfs if merging onto an undefined id', function() {
-            expect(() => reducer(undefined, mergeElements(undefined))).to.throw(Error)
-        })
-
-        it('barfs if merging onto a non-existant id', function() {
-            expect(() => reducer(undefined, mergeElements(1))).to.throw(Error)
+            it('barfs if merging onto a non-existant id', function() {
+                expect(() => reducer(undefined, mergeElements(1))).to.throw(Error)
+            })
         })
     })
 })
