5c7826ad26322ee86b44bb1f03e864ff3d523bdf
Alec Aivazis <alec@aivazis.com>
2017-01-05 01:25:34 -0800

    moved elements selection into its own reducer partial

--- src/store/elements/index.js
+++ src/store/elements/index.js
@@ -3,7 +3,8 @@ import _ from 'lodash'
 // local imports
 import propagatorsPartial from './propagators'
 import anchorsPartial from './anchors'
-import { SELECT_ELEMENTS, CLEAR_SELECTION, MERGE_ELEMENTS } from 'actions/elements'
+import selectionPartial from './selection'
+import { MERGE_ELEMENTS } from 'actions/elements'
 
 // the initial state of elements
 export const initialState = {
@@ -13,37 +14,8 @@ export const initialState = {
     texts: [],
     selection: [],
 }
-// state.propagators.concat(payload)
 
 export default (state = initialState, {type, payload}) => {
-
-    // if the payload represents a new selection
-    if (type === SELECT_ELEMENTS) {
-        // make a deep copy of the state that we can play with
-        const local = _.cloneDeep(state)
-        // clear the selection
-        local.selection = payload.map(selected => {
-            // if there is no element by that type and id
-            if (!state[selected.type][selected.id]) {
-                throw new Error(`Could not find ${selected.type} with id ${selected.id}`)
-            }
-            // pass this item through
-            return selected
-        })
-
-        // return the local copy
-        return local
-    }
-
-    // if the action indicates we need to clear the selection
-    if (type === CLEAR_SELECTION) {
-        // make a deep copy of the state that we can play with
-        const local = _.cloneDeep(state)
-        // clear the selection
-        local.selection = []
-        // return the new state
-        return local
-    }
 
     // if the action indicates we need to dedupe the elements
     if (type === MERGE_ELEMENTS) {
@@ -95,11 +67,11 @@ export default (state = initialState, {type, payload}) => {
         return local
     }
 
-
     // return the updated state
     return {
         ...state,
         anchors: anchorsPartial(state, {type, payload}),
-        propagators: propagatorsPartial(state, {type, payload})
+        propagators: propagatorsPartial(state, {type, payload}),
+        selection: selectionPartial(state, {type, payload}),
     }
 }
