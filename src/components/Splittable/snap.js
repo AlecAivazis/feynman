3324bad0149a420f3613a435847d9051038d5506
Alec Aivazis <alec@aivazis.com>
2017-01-28 12:19:43 -0800

    propagators no longer snap fixed anchors

--- src/components/Splittable/snap.js
+++ src/components/Splittable/snap.js
@@ -11,22 +11,40 @@ export const snapAnchor = ({id, elements, info, setElementAttrs}) => (
 
 export const snapPropagator = ({ setElementAttrs, elements, info:{gridSize}, id}) => {
     // get the anchors assocaited with the propagator we need to snap
-    const {anchor1, anchor2} = elements.propagators[id]
-    // compute the new location for anchor1
-    const anchor1Loc = fixPositionToGrid(elements.anchors[anchor1], gridSize)
-    const anchor2Loc = fixPositionToGrid(elements.anchors[anchor2], gridSize)
+    const {anchor1: anchor1Id, anchor2: anchor2Id} = elements.propagators[id]
+
+    // save references to the two anchors
+    const anchor1 = elements.anchors[anchor1Id]
+    const anchor2 = elements.anchors[anchor2Id]
+    
+
+    // the list of changes
+    const moves = []
+    // if the first anchor is not fixed
+    if (!anchor1.fixed) {
+        // compute the new location for anchor1
+        const anchor1Loc = fixPositionToGrid(anchor1, gridSize)
+
+        // then update its location
+        moves.push({
+            type: 'anchors',
+            id: anchor1Id,
+            ...anchor1Loc,
+        })
+    }
+    // if the second anchor is not fixed
+    if (!anchor2.fixed) {
+        // compute the new location for anchor2
+        const anchor2Loc = fixPositionToGrid(anchor2, gridSize)
+
+        // then update its location
+        moves.push({
+            type: 'anchors',
+            id: anchor2Id,
+            ...anchor2Loc,
+        })
+    }
 
     // update the new location for the anchors
-    setElementAttrs(
-        {
-            type: 'anchors',
-            id: anchor1,
-            ...anchor1Loc,
-        },
-        {
-            type: 'anchors',
-            id: anchor2,
-            ...anchor2Loc,
-        }
-    )
+    setElementAttrs(...moves)
 }
