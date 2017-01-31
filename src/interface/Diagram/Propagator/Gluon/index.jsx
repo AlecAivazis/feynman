fe184013d7b4aa55a55dc3efa36f76b137f4b413
alec aivazis <alec@aivazis.com>
2017-01-31 01:31:55 -0800

    added gluon with end caps

--- src/interface/Diagram/Propagator/Gluon/index.jsx
+++ src/interface/Diagram/Propagator/Gluon/index.jsx
@@ -3,82 +3,15 @@ import React from 'react'
 import SvgMatrix from 'svg-matrix'
 // local imports
 import range from 'utils/range'
-import Align from '../Align'
+import With from './withEndCaps'
+import Without from './withoutEndcaps'
 
-const Gluon = ({
-    x1, y1, x2, y2, direction=1,
-    // pulled out to prevent cascade
-    arrow, anchor1, anchor2,
-    ...unusedProps
-}) => {
-    // compute the length of the line
-    const dx = x2 - x1
-    const dy = y2 - y1
-    const length = Math.sqrt(dx*dx + dy*dy)
+// which gluon we show depends on wether there are end caps or not
+const Gluon = ({endcaps, ...unusedProps}) => endcaps ? <With {...unusedProps} /> : <Without {...unusedProps} />
 
-    // the width of the pattern
-    const scale = 10
-    // the height of the pattern
-    const amplitude = dx < 0 ? scale : - scale
-
-    // find the closest number of full loops
-    const nLoops = Math.round(length / scale / 2)
-
-    // the running counters
-    let cx = x1
-    let cy = y1
-    // the top and bottom limts for the pattern
-    const ymin = cy
-    const ymax = cy + 2 * direction * amplitude
-
-    //  if there isn't enough room for at least one loop
-    if (nLoops === 0) {
-        // render a fermion instead
-        return null
-    }
-
-    // start the path off
-    let pathString = `M ${cx} ${cy} `
-
-    // the step size
-    const delta = scale
-
-    // the first loop has to be drawn separately since it starts with a C
-    pathString += `C ${cx+2*scale} ${ymin} ${cx+2*scale} ${ymax} ${cx+delta} ${ymax} `
-    // update the running total
-    cx += delta
-    // the second half
-    pathString += `S ${cx-delta} ${ymin} ${cx+delta} ${ymin} `
-    // update the running total
-    cx += delta
-
-    // draw the rest of the loops
-    for(let cycle = 1; cycle < nLoops ; cycle++) {
-        // add the first half of the loop
-        pathString += `S ${cx+2*scale} ${ymax} ${cx+delta} ${ymax} `
-        // update the running counter
-        cx += delta
-        // second half of the loop
-        pathString += `S ${cx-delta} ${ymin} ${cx+delta} ${ymin} `
-        // update the running counter
-        cx += delta
-    }
-
-    return (
-        <Align
-            x1={x1}
-            x2={x2}
-            y1={y1}
-            y2={y2}
-            length={2 * scale * nLoops}
-        >
-            <path
-                {...unusedProps}
-                fill="none"
-                d={pathString}
-            />
-        </Align>
-    )
+Gluon.defaultProps = {
+    endcaps: true,
+    direction: 1,
 }
 
 export default Gluon
---
+++ src/interface/Diagram/Propagator/Gluon/withoutEndcaps.jsx
@@ -0,0 +1,82 @@
+// external imports
+import React from 'react'
+// local imports
+import Align from '../Align'
+
+
+const GluonWithoutEndcaps = ({
+    x1, y1, x2, y2, direction,
+    // pulled out to prevent cascade
+    arrow, anchor1, anchor2,
+    ...unusedProps
+}) => {
+    // compute the length of the line
+    const dx = x2 - x1
+    const dy = y2 - y1
+    const length = Math.sqrt(dx*dx + dy*dy)
+
+    // the width of the pattern
+    const scale = 10
+    // the height of the pattern
+    const amplitude = dx < 0 ? scale : - scale
+
+    // find the closest number of full loops
+    const nLoops = Math.round(length / scale / 2)
+
+    // the running counters
+    let cx = x1
+    let cy = y1
+    // the top and bottom limts for the pattern
+    const ymin = cy
+    const ymax = cy + 2 * direction * amplitude
+
+    //  if there isn't enough room for at least one loop
+    if (nLoops === 0) {
+        // render a fermion instead
+        return null
+    }
+    // the step size
+    const delta = scale
+
+    // start the path off
+    let pathString = `M ${cx} ${cy} `
+
+    // the first loop has to be drawn separately since it starts with a C
+    pathString += `C ${cx+2*scale} ${ymin} ${cx+2*scale} ${ymax} ${cx+delta} ${ymax} `
+    // update the running total
+    cx += delta
+    // the second half
+    pathString += `S ${cx-delta} ${ymin} ${cx+delta} ${ymin} `
+    // update the running total
+    cx += delta
+
+    // draw the rest of the loops
+    for(let cycle = 1; cycle < nLoops ; cycle++) {
+        // add the first half of the loop
+        pathString += `S ${cx+2*scale} ${ymax} ${cx+delta} ${ymax} `
+        // update the running counter
+        cx += delta
+        // second half of the loop
+        pathString += `S ${cx-delta} ${ymin} ${cx+delta} ${ymin} `
+        // update the running counter
+        cx += delta
+    }
+
+    return (
+        <Align
+            x1={x1}
+            x2={x2}
+            y1={y1}
+            y2={y2}
+            length={2 * scale * nLoops}
+        >
+            <path
+                {...unusedProps}
+                fill="none"
+                d={pathString}
+            />
+        </Align>
+    )
+}
+
+export default GluonWithoutEndcaps
