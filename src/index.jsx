030a4ffcddd09c88e2307778b67e166cd4c6f254
Alec Aivazis <alec@aivazis.com>
2017-02-19 12:54:19 -0800

    starting elements are now produced with a pattern

--- src/index.jsx
+++ src/index.jsx
@@ -7,40 +7,42 @@ import 'babel-polyfill'
 import App from './interface'
 import store from './store'
 
-import { addAnchors, addPropagators } from 'actions/elements'
+import { placeElement } from 'actions/elements'
 
-store.dispatch(addAnchors(
-    {
-        id: 1,
-        x: 50,
-        y: 100,
-    },
-    {
-        id: 2,
-        x: 100,
-        y: 150,
-    },
-    {
-        id: 3,
-        x: 400,
-        y: 200,
-    },
-))
-
-store.dispatch(addPropagators(
-    {
-        kind: 'gluon',
-        id: 1,
-        anchor1: 1,
-        anchor2: 2,
-    },
-    {
-        kind: 'fermion',
-        id: 2,
-        anchor1: 1,
-        anchor2: 3,
-    }
-))
+store.dispatch(placeElement({
+    type: "pattern",
+    anchors: [
+        {
+            id: 1,
+            x: 50,
+            y: 100,
+        },
+        {
+            id: 2,
+            x: 100,
+            y: 150,
+        },
+        {
+            id: 3,
+            x: 400,
+            y: 200,
+        }
+    ],
+    propagators: [
+        {
+            kind: 'gluon',
+            id: 1,
+            anchor1: 1,
+            anchor2: 2,
+        },
+        {
+            kind: 'fermion',
+            id: 2,
+            anchor1: 1,
+            anchor2: 3,
+        }
+    ]
+}))
 
 ReactDOM.render((
     <Provider store={store}>
