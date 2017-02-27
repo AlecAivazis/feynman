f6687ea1c0f479febe32e583937a4dc5b324dd2c
alec aivazis <alec@aivazis.com>
2017-02-26 21:21:48 -0800

    added drell yen elements

--- src/interface/PatternModal/patterns/dy/index.js
+++ src/interface/PatternModal/patterns/dy/index.js
@@ -1,5 +1,6 @@
 // local imports
 import image from './image.png'
+import { blue } from 'colors'
 
 export default {
     name: "Drell-Yan",
@@ -14,8 +15,28 @@ export default {
             },
             {
                 id: 2,
-                x: 100,
+                x: 150,
                 y: 200
+            },
+            {
+                id: 3,
+                x: 50,
+                y: 300
+            },
+            {
+                id: 4,
+                x: 400,
+                y: 100,
+            },
+            {
+                id: 5,
+                x: 300,
+                y: 200,
+            },
+            {
+                id: 6,
+                x: 400,
+                y: 300
             }
         ],
         propagators: [
@@ -24,6 +45,31 @@ export default {
                 kind: 'fermion',
                 anchor1: 2,
                 anchor2: 1,
+            },
+            {
+                id: 2,
+                kind: 'fermion',
+                anchor1: 3,
+                anchor2: 2,
+            },
+            {
+                id: 3,
+                kind: 'em',
+                stroke: blue,
+                anchor1: 2,
+                anchor2: 5,
+            },
+            {
+                id: 4,
+                kind: 'fermion',
+                anchor1: 4,
+                anchor2: 5,
+            },
+            {
+                id: 5,
+                kind: 'fermion',
+                anchor1: 5,
+                anchor2: 6,
             }
         ]
     }
