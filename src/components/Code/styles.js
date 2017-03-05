347afb2c18b5a535b336fcb0b2185360b0cf53a9
Alec Aivazis <alec@aivazis.com>
2017-03-04 16:35:31 -0800

    began implementing content for export modal

---
+++ src/components/Code/styles.js
@@ -0,0 +1,9 @@
+export default {
+    container: {
+        backgroundColor: 'white',
+        margin: '10px 0',
+        borderRadius: 6,
+        fontFamily: 'monaco, Consolas, Lucida Console, monospace',
+        padding: 10,
+    }
+}
--- src/interface/ExportModal/index.jsx
+++ src/interface/ExportModal/index.jsx
@@ -2,18 +2,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 // local imports
-import { Overlay } from 'components'
+import { Overlay, Code } from 'components'
 import { toggleExportModal } from 'actions/info'
 import styles from './styles'
 
-const ExportModal = ({hideModal}) => (
+const ExportModal = ({elements, hideModal}) => (
     <Overlay title="Export to LaTeX" hide={hideModal}  style={styles.container}>
-        hello
+        Add this to your preamble
+        <Code>
+            \usepackage{'{feynman}'}
+        </Code>
+        Here's your diagram:
+        <Code>
+            \begin{'{feynman}'} <br/>
+            {elements.propagators && Object.values(elements.propagators).map(propagator => (
+                <div key={propagator.id}>
+                    &nbsp;v&nbsp;&nbsp;&nbsp;\{propagator.type}[hello]
+                </div>
+            ))}
+            \end{'{feynman}'}<br/>
+        </Code>
+
     </Overlay>
 )
 
 const mapDispatchToProps = dispatch => ({
     hideModal: () => dispatch(toggleExportModal())
 })
-
-export default connect(null, mapDispatchToProps)(ExportModal)
+const selector = ({elements}) => ({elements})
+export default connect(selector, mapDispatchToProps)(ExportModal)
