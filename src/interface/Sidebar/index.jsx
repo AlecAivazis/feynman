4407a7f61aa55409f1e91d1474eb8f18ca15f5b4
alec aivazis <alec@aivazis.com>
2017-03-25 23:21:11 -0700

    reordered zoom and grid controls

--- src/interface/Sidebar/index.jsx
+++ src/interface/Sidebar/index.jsx
@@ -14,8 +14,8 @@ import ButtonGrid from './ButtonGrid'
 const Sidebar = ({style, dispatch, info}) => (
     <aside style={{...styles.container, ...style}}>
         <TitleControl style={styles.element} />
-        <ZoomLevelControl style={styles.element}/>
         <GridSizeControl style={styles.element}/>
+        <ZoomLevelControl style={styles.element}/>
         <ButtonGrid style={styles.elementWithBorder} />
         <HistorySummary style={styles.elementWithBorder} />
         <HotkeySummary style={styles.element} />
