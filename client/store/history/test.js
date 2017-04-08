dc8984b967c18a2291779aae6ee8c9b391e57867
alec aivazis <alec@aivazis.com>
2017-04-08 16:16:15 -0700

    goto and redo tests pass

--- client/store/history/test.js
+++ client/store/history/test.js
@@ -103,27 +103,39 @@ describe('Reducers', () => {
         test('redo reduces the head bumps the head by one and mutates the state', function() {
             // perform a mutation
             const mutated = wrapped(initial, reducerAction('moon'))
-            const {history: _, ...state} = mutated
 
             // commit the new state, return to the original location, and then move forward one
             const committed = wrapped(mutated, commit('test msg'))
             const undoState = wrapped(committed, undo())
+
             const {history, ...redoState} = wrapped(undoState, redo())
 
-            // make sure the head has been increased (this wont exist so dont go further)
-            expect(history.get('head')).toEqual(-1)
+            // make sure the head has been increased back to the most recent change
+            expect(history.get('head')).toEqual(0)
             // make sure we are back where we belong
-            expect(redoState).toEqual(state)
+            expect(redoState).toEqual(_.omit(committed, 'history'))
         })
 
         test('goto sets the head to a specific index', () => {
-            // perform the redo
-            const redoState = wrapped(initial, goto(2))
-            // the head of the log
-            const head = redoState.history.get('head')
+            // perform and commit a mutation
+            const mutated = wrapped(initial, reducerAction('first state'))
+            const committed = wrapped(mutated, commit('first msg'))
+            // perform and commit a mutation
+            const mutated2 = wrapped(committed, reducerAction('second state'))
+            const committed2 = wrapped(mutated2, commit('second msg'))
+            // perform and commit a mutation
+            const mutated3 = wrapped(committed2, reducerAction('third state'))
+            const committed3 = wrapped(mutated3, commit('third msg'))
 
-            // make sure the head has been increased (this wont exist so dont go further)
+            // go to the state 2 commits ago
+            const gotoState = wrapped(committed3, goto(2))
+            // the head of the log
+            const head = gotoState.history.get('head')
+
+            // make sure the head has been set
             expect(head).toEqual(2)
+            // make sure the state of the reducer is what we expect
+            expect(_.omit(gotoState, 'history')).toMatchObject(_.omit(committed, 'history'))
         })
     })
 })
