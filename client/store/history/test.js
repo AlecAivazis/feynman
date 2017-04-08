eca1cef1b2469978563e40ba143b644d9a0ace31
alec aivazis <alec@aivazis.com>
2017-04-08 14:14:14 -0700

    added failing tests for history enhancer

--- client/store/history/test.js
+++ client/store/history/test.js
@@ -2,13 +2,15 @@
 import { Stack, Map, fromJS } from 'immutable'
 // local imports
 import historyEnhancer from '.'
+import { commit, undo, redo, goto } from 'actions/history'
 
 describe('Reducers', () => {
     describe('History Store Enhancer', () => {
         // the initial state of our mock wrapped reducer
         const reducerInitial = {hello: 'world'}
         // the reducer to wrap
-        const reducer = (state, action) => reducerInitial
+        const reducer = (state=reducerInitial, {type, payload}) => type === 'test' ? ({...state, hello: payload}) : state
+        const reducerAction = payload => ({type: 'test', payload})
         // wrap the reducer
         const wrapped = historyEnhancer(reducer)
 
@@ -17,30 +19,77 @@ describe('Reducers', () => {
 
         beforeEach(() => {
             // the initial state
-            initial = wrapped()
+            initial = wrapped(undefined, {})
         })
 
         test('silently wraps a reducer', () => {
+            const wrappedState = wrapped(reducerInitial, reducerAction('moon'))
+            const reducerState = reducer(reducerInitial, reducerAction('moon'))
             // make sure the result is the same as the unwrapped version
-            expect(wrapped('hello', 'world')).toMatchObject(reducer('hello', 'world'))
-        })
-
-        test('injects the history state alongside the underlying store', () => {
-            // make sure there is an underlying history entry
-            expect(initial).toMatchObject({
-                history: {},
-            })
+            expect(wrappedState).toMatchObject(reducerState)
         })
 
         test('head and log accomodate reducer initial state', () => {
             expect(initial.history).toEqual(Map({
-                head: 1,
-                log: Stack.of([fromJS(reducerInitial)])
+                head: 0,
+                log: Stack.of([
+                    Map({
+                        message: '',
+                        state: fromJS(reducerInitial),
+                    })
+                ])
             }))
         })
 
         test('committing a new state bumps the head and adds the current state to the log', () => {
+            // mutate the state
+            const mutated = wrapped(initial, reducerAction('after'))
+            // pull out the history from the state before the commit
+            const {history, ...before} = mutated
 
+            // commit the new state
+            const committed = wrapped(mutated, commit('test msg'))
+
+            // the head of the log
+            const headIndex = committed.history.get('head')
+            const head = committed.history.get('log').get(head)
+
+            // make sure the head has been increased
+            expect(headIndex).toEqual(1)
+            // make sure the head of the log has the right state
+            expect(head.get('state')).toMatchObject(before)
+        })
+
+        test('undo reduces the head by one', () => {
+            // undo
+            const undoState = wrapped(initial, undo())
+            // commit the new state (slightly sloppy but avoids false positive test suite)
+            const committed = wrapped(undoState, commit('test msg'))
+            // the head of the log
+            const head = committed.history.get('head')
+
+            // make sure the head has been increased
+            expect(head).toEqual(1)
+        })
+
+        test('redo bumps the head bumps the head by one', function() {
+            // perform the redo
+            const redoState = wrapped(initial, redo())
+            // the head of the log
+            const head = redoState.history.get('head')
+
+            // make sure the head has been increased (this wont exist so dont go further)
+            expect(head).toEqual(1)
+        })
+
+        test('goto sets the head to a specific index', () => {
+            // perform the redo
+            const redoState = wrapped(initial, goto(2))
+            // the head of the log
+            const head = redoState.history.get('head')
+
+            // make sure the head has been increased (this wont exist so dont go further)
+            expect(head).toEqual(2)
         })
     })
 })
