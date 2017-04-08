9ac3f230e25ee6287e7e725d51e350cbc6b1a629
alec aivazis <alec@aivazis.com>
2017-04-08 14:56:53 -0700

    added history commit

--- client/store/history/index.js
+++ client/store/history/index.js
@@ -1,31 +1,68 @@
 // external imports
 import { Map, Stack, fromJS } from 'immutable'
+// local imports
+import {
+    COMMIT,
+    UNDO,
+    REDO,
+    GOTO,
+} from 'actions/history'
+
+// the default configuration
+const defaultConfig = {
+    initialMessage: ''
+}
 
 // history is implemented as a store enhancer (higher-order reducer)
-export default function historyEnhancer(reducer) {
+export default function historyEnhancer(reducer, config = defaultConfig) {
     // the reducer's initial state
     const wrappedInitial = reducer(undefined, {})
     // the initial state of the enhanced reducer
     const initialState = {
         history: Map({
             head: 0,
-            log: Stack.of([
+            log: Stack.of(
                 Map({
-                    message: '',
+                    message: config.initialMessage,
                     state: fromJS(wrappedInitial),
                 })
-            ])
+            )
         })
     }
 
-    return (state = initialState, action) => {
-        // the next state of the store
-        const next = reducer(state, action)
+    return ({history: oldHistory, ...state} = initialState, {type, payload}) => {
 
-        // mix the history state into the wrapped reducer
+        // the next state of the store
+        const next = reducer(state, {type, payload})
+
+        // if we have to commit a new state to the log
+        if (type === COMMIT) {
+            // the new entry in the commit log
+            const entry = Map({
+                message: payload,
+                state,
+            })
+            // the current head
+            const head = oldHistory.get('head')
+            // the log after the commit needs to include this entry and clear everything after
+            const log = oldHistory.get('log')
+                            // clear everything after the current head
+                            .slice(0, head + 1)
+                            .push(entry)
+
+            // return the previous state with the current one appended to the log
+            return {
+                ...state,
+                history: oldHistory
+                            .set('head', head + 1)
+                            .set('log', log)
+            }
+        }
+
+        // we didn't change anything so just pass along whatever the wrapper reducer gave us
         return {
-            ...state,
             ...next,
+            history: oldHistory,
         }
     }
 }
--- client/store/history/test.js
+++ client/store/history/test.js
@@ -32,27 +32,41 @@ describe('Reducers', () => {
         test('head and log accomodate reducer initial state', () => {
             expect(initial.history).toEqual(Map({
                 head: 0,
-                log: Stack.of([
+                log: Stack.of(
                     Map({
                         message: '',
                         state: fromJS(reducerInitial),
                     })
-                ])
+                )
+            }))
+        })
+
+        test('can set initial message', () => {
+            // wrap the reducer
+            const reducerWithMessage = historyEnhancer(reducer, {initialMessage: "hello"})
+            initial = reducerWithMessage(undefined, {})
+
+            expect(initial.history).toEqual(Map({
+                head: 0,
+                log: Stack.of(
+                    Map({
+                        message: "hello",
+                        state: fromJS(reducerInitial),
+                    })
+                )
             }))
         })
 
         test('committing a new state bumps the head and adds the current state to the log', () => {
-            // mutate the state
+            // mutate the state and commit it
             const mutated = wrapped(initial, reducerAction('after'))
-            // pull out the history from the state before the commit
-            const {history, ...before} = mutated
-
-            // commit the new state
-            const committed = wrapped(mutated, commit('test msg'))
+            // save the user state before we committed
+            const { history: _, ...before } = mutated
+            const { history } = wrapped(mutated, commit('test msg'))
 
             // the head of the log
-            const headIndex = committed.history.get('head')
-            const head = committed.history.get('log').get(head)
+            const headIndex = history.get('head')
+            const head = history.get('log').peek(headIndex)
 
             // make sure the head has been increased
             expect(headIndex).toEqual(1)
@@ -60,26 +74,35 @@ describe('Reducers', () => {
             expect(head.get('state')).toMatchObject(before)
         })
 
-        test('undo reduces the head by one', () => {
-            // undo
-            const undoState = wrapped(initial, undo())
-            // commit the new state (slightly sloppy but avoids false positive test suite)
-            const committed = wrapped(undoState, commit('test msg'))
-            // the head of the log
-            const head = committed.history.get('head')
+        test('undo reduces the head by one and mutates the state', () => {
+            // perform a mutation
+            const mutated = wrapped(initial, reducerAction('moon'))
+            const {history: _, ...state} = mutated
 
-            // make sure the head has been increased
-            expect(head).toEqual(1)
+            // commit the new state and return to the original location
+            const committed = wrapped(mutated, commit('test msg'))
+            const {history, ...undoState} = wrapped(committed, undo())
+
+            // make sure we are back where we started
+            expect(undoState).toEqual(reducerInitial)
+            // make sure the head has been increased (this wont exist so dont go further)
+            expect(history.get('head')).toEqual(1)
         })
 
-        test('redo bumps the head bumps the head by one', function() {
-            // perform the redo
-            const redoState = wrapped(initial, redo())
-            // the head of the log
-            const head = redoState.history.get('head')
+        test('redo bumps the head bumps the head by one and mutates the state', function() {
+            // perform a mutation
+            const mutated = wrapped(initial, reducerAction('moon'))
+            const {history: _, ...state} = mutated
+
+            // commit the new state, return to the original location, and then move forward one
+            const committed = wrapped(mutated, commit('test msg'))
+            const undoState = wrapped(committed, undo())
+            const {history, ...redoState} = wrapped(undoState, redo())
 
             // make sure the head has been increased (this wont exist so dont go further)
-            expect(head).toEqual(1)
+            expect(history.get('head')).toEqual(2)
+            // make sure we are back where we belong
+            expect(redoState).toEqual(state)
         })
 
         test('goto sets the head to a specific index', () => {
